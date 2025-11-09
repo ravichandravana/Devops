610f7e623229b900b34bd21cecc0f311a6f1bd58
root <root@ip-172-31-3-218.eu-north-1.compute.internal>
2025-11-09 13:27:04 +0000

    code updatd

--- app.java
+++ app.java
@@ -3,6 +3,7 @@ public class App{
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("editing from git");
+                System.out.println("Dev1 code");
 		System.out.println("Dev 2 code");
 	}
 }
