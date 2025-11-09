30b6d6042fb12df985366765180e9a16f3915de5
ravichandravana <vanaravi53@gmail.com>
2025-11-09 18:48:08 +0530

    Add new print statement for 'Dev 2 code'

--- app.java
+++ app.java
@@ -3,5 +3,6 @@ public class App{
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("editing from git");
+		System.out.println("Dev 2 code");
 	}
 }
